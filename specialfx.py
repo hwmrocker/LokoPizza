4100a02a118e07c31ae34e2d029754eb8ae27002
Olaf Gladis <olaf@gladis.org>
2014-12-20 17:16:03 +0100

    bomb bugs fixed

--- specialfx.py
+++ specialfx.py
@@ -3,33 +3,26 @@ from time import sleep
 
 def explosion(y, x, game):
     screen = game.lokopizza.screen
-    screen.addstr(y, x, " ")
-    screen.refresh()
-    # sleep(0.3)
-    yield None
-    screen.addstr(y, x, " ")
-    screen.refresh()
-    yield None
-    screen.addstr(y, x, " ")
-    screen.refresh()
-    yield None
+    for _ in range(3):
+        screen.addstr(y, x, " ")
+        screen.refresh()
+        # sleep(0.3)
+        yield None
     
     oldchar1 = screen.instr(y+1, x+1)
     oldchar2 = screen.instr(y-1, x+1)
     oldchar3 = screen.instr(y-1, x-1)
     oldchar4 = screen.instr(y+1, x-1)
     
-    screen.addstr(y+1, x+1, "*")
-    screen.addstr(y-1, x+1, "*")
-    screen.addstr(y-1, x-1, "*")
-    screen.addstr(y+1, x-1, "*")
-    screen.addstr(y, x, " ")
-    screen.refresh()
-    # sleep(0.2)
-    yield None
-    screen.addstr(y, x, " ")
-    screen.refresh()
-    yield None
+    for _ in range(2):
+        screen.addstr(y+1, x+1, "*")
+        screen.addstr(y-1, x+1, "*")
+        screen.addstr(y-1, x-1, "*")
+        screen.addstr(y+1, x-1, "*")
+        screen.addstr(y, x, " ")
+        screen.refresh()
+        # sleep(0.2)
+        yield None
 	
     oldchar11 = screen.instr(y+2, x+2)
     oldchar22 = screen.instr(y-2, x+2)
